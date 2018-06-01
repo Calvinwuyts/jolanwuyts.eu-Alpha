b60802bb34311ef5eb8e7e54082eff576a7d08b6
Calvinwuyts <jolanwuyts@gmail.com>
2018-06-01 16:10:25 +0200

    finished carousel 3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 
 $(document).ready(function(){
-    console.log('testing jquery');
+    console.log('pausing animation');
+    $(".skills").css("animation-play-state", "paused");
     $(changeback);
     $('#carousel').slick({
         accessibility: true,
         adaptiveHeight: true,
-        dots:true
+        dots:true,
+        lazyload:'progressive'
     });
     console.log('slick initialized');
 });
@@ -21,6 +23,8 @@ function changeback(){
             console.log(slide);
             $('html').css({'background': "url('../newwebsite/images/greenseaback.png') repeat"});
         } else if (slide === 2) {
+            console.log("playing animation");
+            $(".skills").css("animation-play-state", "running");
             $('html').css({'background': "url('../newwebsite/images/redseaback.png') repeat"});
         } else if (slide === 3) {
             $('html').css({'background': "url('../newwebsite/images/pinkseaback.png') repeat"});
